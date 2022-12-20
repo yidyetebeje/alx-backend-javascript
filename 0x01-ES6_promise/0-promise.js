8ce44e2c1d997a069ec4021ce85793233b33020e
yidye <yidnekachewtebeje@gmail.com>
2022-12-20 10:58:21 -0800

    fix style issues

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,5 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    if (true) {
-      resolve('Success');
-    } else {
-      reject(Error('The fake API is not working currently'));
-    }
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve('success');
   });
 }
