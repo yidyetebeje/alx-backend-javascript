c6dc3ffb1bac6814b4223d3ecb628ff20c368d03
yidye <yidnekachewtebeje@gmail.com>
2022-12-22 15:05:58 -0800

    edited

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,10 +1,22 @@
+import Car from './10-car';
+
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range;
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
   }
 
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color, this._range);
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
   }
 }
