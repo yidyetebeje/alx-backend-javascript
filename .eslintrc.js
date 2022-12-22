738b1b15ae6fcfd92cb4f177903933ab563d87b1
yidye <yidnekachewtebeje@gmail.com>
2022-12-22 14:22:36 -0800

    formatted

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,10 @@
-module.exports = {
-    env: {
+{
+   env: {
         browser: false,
         es6: true,
         jest: true,
     },
-    extends: [
-        'airbnb-base',
-        'plugin:jest/all',
-    ],
-    globals: {
+  globals: {
         Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',
     },
@@ -16,20 +12,18 @@ module.exports = {
         ecmaVersion: 2018,
         sourceType: 'module',
     },
-    plugins: ['jest'],
-    rules: {
-        'no-console': 'off',
+  "extends": ["prettier", "airbnb-base"],
+  "plugins": ["prettier"],
+  "rules": {
+    "prettier/prettier": ["error"],
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
         'no-shadow': 'off',
         'no-restricted-syntax': [
             'error',
             'LabeledStatement',
             'WithStatement',
         ],
-    },
-    overrides: [
-        {
-            files: ['*.js'],
-            excludedFiles: 'babel.config.js',
-        }
-    ]
-};
+  }
+}
