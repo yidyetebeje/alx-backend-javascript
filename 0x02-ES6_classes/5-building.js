07bf5387521754d861ce2c2f618ab109825a9b18
yidye <yidnekachewtebeje@gmail.com>
2022-12-22 14:29:10 -0800

    updates

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+  get sqft() {
+    return this._sqft;
+  }
+  get area() {
+    return this._sqft;
+  }
+  set area(area) {
+    this._sqft = area;
+  }
+}
