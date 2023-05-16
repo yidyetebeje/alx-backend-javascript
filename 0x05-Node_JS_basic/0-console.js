f74f08203c7dddd31f0fc0806fe1079e45554175
Yidnekachew Tebeje <yidnekachewtebeje@gmail.com>
2023-05-16 22:27:16 +0300

    updates

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,9 @@
-function dispalyMessage(msg){
-    process.stdout.write(msg);
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
 };
 
-module.exports = dispalyMessage;
+module.exports = displayMessage;
